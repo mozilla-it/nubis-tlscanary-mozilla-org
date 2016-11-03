6d769f7a3784483a10cef6fb4b1253378565f197
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-03 14:51:28 -0400

    tyop

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -25,8 +25,7 @@ class {
         proxy_ips => [ '127.0.0.1', '10.0.0.0/8' ];
 }
 
-apache::vhost {
-  $::vhost_name:
+apache::vhost { 'tlscanary':
     port              => 80,
     default_vhost     => true,
     docroot           => '/var/www/html',
