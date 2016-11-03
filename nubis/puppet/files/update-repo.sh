f913faa3cc91f3e10b63706d6068dc6539d6f5c9
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-03 12:10:01 -0400

    Big simplification of the code update/deploy logic
    Overall:
     - Move git checkout to /var/lib/ssl_compat instead of /tmp
     - Keep /var/lib/ssl_compat clean with proper permissions
     - Use rsync --delete to move from /var/lib/ssl_compat to /var/www/html
    
    Build:
     - Get rid of the submodule, since it's redundant and we don't really bake it in
     - Just call update-site.sh once during build time instead
    
    Fixes #12

--- nubis/puppet/files/update-repo.sh
+++ nubis/puppet/files/update-repo.sh
@@ -1,28 +1,31 @@
 #!/bin/bash -l
 # Update the local copy of this site's git repository
-# Copy the contents to the web root.
 #
 
 SOURCE_REPO=https://github.com/mwobensmith/ssl_compat.git
-LOCAL_REV=$(/usr/bin/git rev-parse master)
-REMOTE_REV=$(/usr/bin/git rev-parse origin/master)
+REPO_DIR=/var/lib/ssl_compat
 
-cd /tmp
+mkdir -p $REPO_DIR
 
 # Clone the repository if the local repo is empty or non-existent
-if [ ! -d /tmp/ssl_compat/.git ]
+if [ ! -d $REPO_DIR/.git ]
 then
-  rm -rf /tmp/ssl_compat
-  /usr/bin/git clone $SOURCE_REPO
+  rm -rf $REPO_DIR
+  /usr/bin/git clone $SOURCE_REPO $REPO_DIR
 fi
 
 # Otherwise, let's just git pull when needed
-cd /tmp/ssl_compat
+cd $REPO_DIR
 /usr/bin/git remote update
+
+LOCAL_REV=$(/usr/bin/git rev-parse master)
+REMOTE_REV=$(/usr/bin/git rev-parse origin/master)
 
 # git pull if master and origin/master do not match
 # The local repository should never be ahead of origin/master
 if [ $LOCAL_REV != $REMOTE_REV ]
 then
   /usr/bin/git pull
+  /usr/bin/git describe --always --tags --dirty > _revision.info
 fi
+
