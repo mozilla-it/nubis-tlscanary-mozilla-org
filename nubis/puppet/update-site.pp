4ac0f237b43a2a8ddd7ed56596af8a4cdf0df67c
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-03 15:19:16 -0400

    last tyop of the day

--- nubis/puppet/update-site.pp
+++ nubis/puppet/update-site.pp
@@ -28,8 +28,7 @@ file { "/etc/nubis.d/${project_name}":
   owner   => root,
   group   => root,
   mode    => '0755',
-  content => "
-#!/bin/bash -l
+  content => "#!/bin/bash -l
 # Runs once on instance boot, after all infra services are up and running
 
 # Pull latest version
