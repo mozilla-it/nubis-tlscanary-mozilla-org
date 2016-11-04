f7fe5130a5d1376c94bbb51adce2a5d2290de2f9
Philippe M. Chiasson <gozer@mozilla.com>
2016-11-04 11:09:38 -0400

    Support amazon-linux and ubuntu with git pinning (#20)
    * Support amazon-linux and ubuntu with git pinning
    
    Since we've been OS agnostic so far, let's try and keep it that way
    
    Improves #16
    
    * puppet-lint++

--- nubis/puppet/git.pp
+++ nubis/puppet/git.pp
@@ -1,7 +1,19 @@
 # Ensure that git is installed
 
+case $::osfamily {
+  'RedHat': {
+    $git_package_version            = '2.7.3-1.46.amzn1'
+  }
+  'Debian', 'Ubuntu': {
+    $git_package_version            = '1:1.9.1-1ubuntu0.3'
+  }
+  default: {
+    fail("Unknown git version for OS ${::osfamily}")
+  }
+}
+
 package {
   'git':
-    ensure => '1:1.9.1-1ubuntu0.3',
+    ensure => $git_package_version,
     name   =>  'git'
 }
