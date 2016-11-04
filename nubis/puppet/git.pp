e72325bbf1a1a55e4dfe9a4bf99abb44823adae7
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-04 10:59:17 -0400

    puppet-lint++

--- nubis/puppet/git.pp
+++ nubis/puppet/git.pp
@@ -1,15 +1,15 @@
 # Ensure that git is installed
 
 case $::osfamily {
-    'RedHat': {
-        $git_package_version            = '2.7.3-1.46.amzn1'
-    }
-    'Debian', 'Ubuntu': {
-        $git_package_version            = '1:1.9.1-1ubuntu0.3'
-     }
-     default: {
-        fail("Unknown git version for OS ${::osfamily}")
-     }
+  'RedHat': {
+    $git_package_version            = '2.7.3-1.46.amzn1'
+  }
+  'Debian', 'Ubuntu': {
+    $git_package_version            = '1:1.9.1-1ubuntu0.3'
+  }
+  default: {
+    fail("Unknown git version for OS ${::osfamily}")
+  }
 }
 
 package {
