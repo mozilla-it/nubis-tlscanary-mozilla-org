d5306cad24208fd4b2d1faff0ffd50fb7a1acfff
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-07 13:32:27 -0500

    Use atomic-rsync to update the site in one atomic pass

---
+++ nubis/puppet/atomic-rsync.pp
@@ -0,0 +1,26 @@
+file { "/usr/local/bin/atomic-rsync":
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  source  => 'puppet:///nubis/files/atomic-rsync',
+}
+
+file { [ "/var/www/html-1", "/var/www/html-2" ]:
+  ensure => 'directory',
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  require => [
+    Package['httpd'],
+  ]
+}
+
+file { "/var/www/html":
+  ensure => 'link',
+  force => true,
+  target => '/var/www/html-2',
+  require => [
+    Package['httpd'],
+  ]
+}
--- nubis/puppet/files/update-site.sh
+++ nubis/puppet/files/update-site.sh
@@ -7,4 +7,4 @@ REPO_DIR=/var/lib/ssl_compat
 /opt/admin-scripts/update-repo.sh
 
 # Write to web root: /var/www/html
-rsync -a --cvs-exclude --delete $REPO_DIR/ /var/www/html/
+/usr/local/bin/atomic-rsync -a --cvs-exclude --delete $REPO_DIR/ /var/www/html/
