8f60de08e8f20f9a040843f51d00f0a63b0fe6c5
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-16 13:32:54 -0500

    WIP: Use fluentd+prometheus to aggregate some counters from Apache logs

--- nubis/puppet/fluentd.pp
+++ nubis/puppet/fluentd.pp
@@ -2,7 +2,21 @@ class { 'fluentd':
   service_ensure => stopped
 }
 
+fluentd::install_plugin { 'prometheus':
+  ensure      => '0.2.1',
+  plugin_name => 'fluent-plugin-prometheus',
+  plugin_type => 'gem',
+}
+
 fluentd::configfile { 'apache': }
+
+fluentd::source { 'apache_prometheus':
+  configfile => 'apache',
+  type       => 'prometheus',
+  config     => {
+    'port'     => '9108',
+  },
+}
 
 fluentd::source { 'apache_access':
   configfile => 'apache',
@@ -25,3 +39,41 @@ fluentd::source { 'apache_error':
     'pos_file' => '/var/log/apache2/error.log.pos',
   },
 }
+
+fluentd::filter { 'apache_prometheus':
+      configfile => 'apache',
+      pattern    => 'forward.apache.access',
+      type       => 'prometheus',
+# lint:ignore:single_quote_string_with_variables
+      config     => {
+        '<metric>' => '
+    name apache_cnt_method_code_path
+    type counter
+    desc Apache requests broken by method, status code and path
+    <labels>
+      method ${method}
+      code   ${code}
+      path   ${path}
+    </labels>
+  </metric>
+  <metric>
+    name apache_cnt_method_code
+    type counter
+    desc Apache requests broken by method and status code
+    <labels>
+      method ${method}
+      code   ${code}
+    </labels>
+  </metric>
+  <metric>
+    name apache_cnt_code
+    type counter
+    desc Apache requests b roken by status code
+    <labels>
+      code   ${code}
+    </labels>
+  </metric>
+',
+# lint:endignore
+      }
+    }
