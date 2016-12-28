b067cfb4065d4a423c8febb297360de1dcea8862
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-28 13:39:38 -0500

    switch to nubis-apache v0.1.1

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -1,39 +1,17 @@
 # Define how Apache should be installed and configured
-# We should try to recycle the puppetlabs-apache puppet module in the future:
-# https://github.com/puppetlabs/puppetlabs-apache
-#
-
-include nubis_discovery
-
-nubis::discovery::service {
-  $project_name:
-    tags     => [ 'apache' ],
-    port     => 80,
-    check    => '/usr/bin/curl -If http://localhost:80',
-    interval => '30s',
-}
 
 $timeout = 120
 
-class {
-    'apache':
-        mpm_module          => 'event',
-        keepalive           => 'On',
-        timeout             => $timeout,
-        keepalive_timeout   => $timeout,
-        default_mods        => true,
-        default_vhost       => false,
-        default_confd_files => false,
-        service_enable      => false,
-        service_ensure      => false;
-    'apache::mod::status':;
-    'apache::mod::remoteip':
-        proxy_ips => [ '127.0.0.1', '10.0.0.0/8' ];
-    'apache::mod::expires':
-        expires_default => 'access plus 30 minutes';
+class { 'nubis_apache':
+  timeout => $timeout,
+  update_script_source => 'puppet:///nubis/files/update-site.sh',
+  update_script_interval => {
+    minute => [ fqdn_rand(30), ( fqdn_rand(30) + 30 ) % 60],
+  },
 }
 
-apache::vhost { 'tlscanary':
+apache::vhost { $project_name:
+    serveradmin        => 'webops@mozilla.com',
     port               => 80,
     default_vhost      => true,
     docroot            => '/var/www/html',
--- nubis/puppet/files/update-site.sh
+++ nubis/puppet/files/update-site.sh
@@ -3,8 +3,40 @@
 #
 
 REPO_DIR=/var/lib/ssl_compat
+SOURCE_REPO=https://github.com/mwobensmith/ssl_compat.git
 
-/opt/admin-scripts/update-repo.sh
+# Ensure the repo is tidy
+cleanup () {
+  cd $REPO_DIR || exit
+  /usr/bin/git describe --always --tags --dirty > _revision.txt
+  find $REPO_DIR -type d \! -perm 0755 -exec chmod 0755 {} \;
+  find $REPO_DIR -type f \! -perm 0644 -exec chmod 0644 {} \;
+}
 
-# Write to web root: /var/www/html
-/usr/local/bin/atomic-rsync -a --cvs-exclude --delete $REPO_DIR/ /var/www/html/
+# Clone the repository if the local repo is empty or non-existent
+if [ ! -d $REPO_DIR/.git ]
+then
+  rm -rf $REPO_DIR
+  /usr/bin/git clone $SOURCE_REPO $REPO_DIR
+  cleanup
+fi
+
+# Otherwise, let's just git pull when needed
+cd $REPO_DIR || exit
+/usr/bin/git remote update
+
+LOCAL_REV=$(/usr/bin/git rev-parse master)
+REMOTE_REV=$(/usr/bin/git rev-parse origin/master)
+
+# git pull if master and origin/master do not match
+# The local repository should never be ahead of origin/master
+if [ "$LOCAL_REV" != "$REMOTE_REV" ]
+then
+  /usr/bin/git pull
+  cleanup
+
+  # Write to web root: /var/www/html
+  /usr/local/bin/atomic-rsync -a --cvs-exclude --delete $REPO_DIR/ /var/www/html/
+fi
+
+
